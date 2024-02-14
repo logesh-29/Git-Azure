5de3ec03ca76b6eae6b2614b61f8b77572db4699
LogeshRavi <139251813+logesh-29@users.noreply.github.com>
2024-02-14 14:04:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
+    resource_group_name  = "terraformdemo"
+    storage_account_name = "ciplstorage1"
+    container_name       = "lokicicp"
     key                  = "dev.terraform.tfstate"
   }
 }
